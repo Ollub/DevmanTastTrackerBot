78e4ca3aa4a56c2e646ec683923a488bb61e5a6e
Oleg Orlov <43028500+O3DB@users.noreply.github.com>
2019-08-28 16:58:51 +0300

    Edit docstring for setup_logging()

--- log.py
+++ log.py
@@ -9,7 +9,12 @@ def setup_logging(
     defualt_level=logging.INFO,
     env_key='LOG_CFG'
     ):
-    """Setup logging configuration
+    """Setup logging configuration from logging.yaml config file.
+
+    Keyword arguments:
+    defaulth_path -- config .yaml file path (default logging.yaml)
+    default_level -- default logging level (default logging.INFO)
+    env_key -- environment variable name for logging config file path
     """
     path = os.getenv(env_key, default_path)
     if os.path.exists(path):
