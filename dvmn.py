5ea550ba6d96dc07978c8a76550b63f040a14a68
Oleg Orlov <43028500+O3DB@users.noreply.github.com>
2019-08-29 09:58:15 +0300

    Update poll_dvmn_lesson_info

--- dvmn.py
+++ dvmn.py
@@ -9,10 +9,13 @@ from settings import CONFIG
 logger = logging.getLogger(__name__)
 
 
-def poll_dvmn_lesson_info(timestamp=None, timeout=120):
-    '''
-    Function polls DVMN API till get lesson info
-    '''
+def poll_dvmn_lesson_info(timestamp=None, timeout=100):
+    """Function polls DVMN API for getting lesson info
+
+    Keyword arguments:
+    timestamp - timestamp of last response from server
+    timeout - typical timout for dvmn server 90sec, but it can change
+    """
     url = 'https://dvmn.org/api/long_polling/'
     headers = {"Authorization": f"Token {CONFIG['DVMN_TOKEN']}"}
     params = {'timestamp': timestamp}
@@ -20,14 +23,13 @@ def poll_dvmn_lesson_info(timestamp=None, timeout=120):
     logger.debug(f'Start polling with timestamp {timestamp}')
     start_time = time.time()
     response = requests.get(url, headers=headers, params=params, timeout=timeout)
+    response.raise_for_status()
     logger.debug("Polling time = {}".format(start_time - time.time()))
 
-    if response.json()['status'] == 'timeout':  #no updates ==> continue polling from stop point
-        timestamp = response.json()['timestamp_to_request']
-        poll_dvmn_lesson_info(timestamp)
-    
-    timestamp = response.json()['last_attempt_timestamp']
-    return response.json(), timestamp
+    timestamp = response.json().get('last_attempt_timestamp') or\
+                response.json().get('timestamp_to_request')
+    lessons_info = response.json().get('new_attempts')
+    return lessons_info, timestamp
 
 
 def format_lesson_info(lesson_info):
