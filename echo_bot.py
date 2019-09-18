8bfb9ed496d1a86f6807bff2273d9fc990ee1709
Oleg Orlov <43028500+O3DB@users.noreply.github.com>
2019-09-18 18:14:26 +0300

    test echo 1

---
+++ echo_bot.py
@@ -0,0 +1,37 @@
+from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
+import logging
+
+from settings import CONFIG
+
+
+logging.basicConfig(format='%(name)s - %(levelname)s - %(message)s',
+                    level=logging.INFO,
+                    filename='bot.log'
+                    )
+
+
+def greet_user(bot, update):
+    text = 'Вызван /start'
+    print(text)
+    update.message.reply_text(text)
+
+
+def talk_to_me(bot, update):
+    user_text = update.message.text
+    print(user_text)
+    update.message.reply_text(user_text)
+
+
+def main():
+    mybot = Updater(token = CONFIG["BOT_TOKEN"], request_kwargs=CONFIG['PROXY'])
+
+    dp = mybot.dispatcher
+    dp.add_handler(CommandHandler("start", greet_user))
+    dp.add_handler(MessageHandler(Filters.text, talk_to_me))
+
+    mybot.start_polling()
+    mybot.idle()
+
+
+if __name__ == '__main__':
+    main()
