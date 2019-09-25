a6786c375bb711fb97d97d9300983e85a067a9a1
Oleg Orlov <43028500+O3DB@users.noreply.github.com>
2019-09-25 17:10:40 +0300

    Modify logging settings

--- log.py
+++ log.py
@@ -4,6 +4,22 @@ import logging.config
 
 import yaml
 
+class BotHandler(logging.Handler):
+    """
+    Telegram bot handler.
+    Takes instance of telegram.ext.Updater and chat_id as attribute
+    and sends logs as text messages to the chat using updater.
+    """
+    def __init__(self, updater, chat_id, *args, **kwargs):
+        self.updater = updater
+        self.chat_id = chat_id
+        super().__init__(*args, **kwargs)
+
+    def emit(self, record):
+        log_entry = self.format(record)
+        self.updater.bot.send_message(chat_id=self.chat_id, text=log_entry)
+
+
 def setup_logging(
     default_path='logging.yaml',
     defualt_level=logging.INFO,
@@ -23,3 +39,16 @@ def setup_logging(
         logging.config.dictConfig(config)
     else:
         logging.basicConfig(level=defualt_level)
+
+
+def get_logger(updater, chat_id, logger_name, defualt_level=logging.INFO):
+    logger = logging.getLogger(logger_name)
+    formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+
+    bh = BotHandler(updater, chat_id)
+    bh.setLevel(defualt_level)
+    bh.setFormatter(formatter)
+
+    logger.addHandler(bh)
+
+    return logger
--- main.py
+++ main.py
@@ -7,7 +7,8 @@ from requests.exceptions import HTTPError
 
 from settings import CONFIG
 from dvmn import poll_dvmn_lesson_info, format_lesson_info
-from log import setup_logging
+
+from log import setup_logging, get_logger
 
 
 logger = logging.getLogger(__name__)
@@ -18,24 +19,27 @@ dp = updater.dispatcher
 
 def main():
     setup_logging()
+    logger = get_logger(updater, CONFIG['CHAT_ID'], 'my_logger')
     logger.info('Bot started')
+
+    # start polling dvmn API
     timestamp = None
-    #start polling dvmn API
     while True:
         try:
             lessons_info, timestamp = poll_dvmn_lesson_info(timestamp=timestamp)
-            if not lessons_info:
-                continue
-            for lesson_info in lessons_info:
-                logger.info(f'Get lesson info from dvmn.org :\n{lesson_info}')
-                formated_info = format_lesson_info(lesson_info)
-                updater.bot.send_message(chat_id=CONFIG['CHAT_ID'], text=formated_info)
         except ReadTimeout:
             continue
         except ConnectionError:
             logger.error('Connection Error')
         except HTTPError:
             logger.error('HTTPError')
+        except Exception:
+            logger.error('Unexpected exception!', exc_info=True)
+        else:
+            for lesson_info in lessons_info or []:
+                logger.info(f'Get lesson info from dvmn.org :\n{lesson_info}')
+                formated_info = format_lesson_info(lesson_info)
+                updater.bot.send_message(chat_id=CONFIG['CHAT_ID'], text=formated_info)
 
 
 if __name__ == '__main__':
